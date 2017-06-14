91856b65a243ead53e5e7007dd4ae17a20a5734b
Alex Lindroos <alex.lindroos@codemate.com>
2017-06-14 15:01:11 +0300

    first commit

---
+++ app/src/main/java/com/example/codemate/chatboxbox/ChatActivity.kt
@@ -0,0 +1,16 @@
+package com.example.codemate.chatboxbox
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Created by Alex Lindroos on 14/06/2017.
+ */
+
+class ChatActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
---
+++ app/src/main/java/com/example/codemate/chatboxbox/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.codemate.chatboxbox
+
+import android.app.Activity
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val SIGN_IN_REQUEST_CODE = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_start.visibility = View.GONE
+
+        btn_start.setOnClickListener {
+            val intent = Intent (this, ChatActivity::class.java)
+            startActivity(intent)
+        }
+
+        authenticateUser()
+    }
+
+    fun authenticateUser() {
+        if(FirebaseAuth.getInstance().currentUser == null) {
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .build(), SIGN_IN_REQUEST_CODE
+            )
+        } else {
+            Toast.makeText(this, "Welcome " + FirebaseAuth.getInstance()
+                    .currentUser!!.displayName, Toast.LENGTH_LONG)
+                    .show()
+            btn_start.visibility = View.VISIBLE
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                Toast.makeText(this, "Signed in succesfully!", Toast.LENGTH_LONG).show()
+                btn_start.visibility = View.VISIBLE
+            }
+
+        } else {
+            Toast.makeText(this, "We could not sign you in. Please try again later.", Toast.LENGTH_LONG).show()
+            finish()
+        }
+    }
+}
